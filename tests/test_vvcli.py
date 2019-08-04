2d7c219d03be5f55a23a0f8169f39bc656213744
adamboche <adamboche@gmail.com>
2019-08-04 00:45:45 +0000

    Test import

--- tests/test_vvcli.py
+++ tests/test_vvcli.py
@@ -1,11 +1,6 @@
-from click.testing import CliRunner
-
-from vvcli.cli import main
+import vvcli.app
 
 
-def test_main():
-    runner = CliRunner()
-    result = runner.invoke(main, [])
-
-    assert result.output == "()\n"
-    assert result.exit_code == 0
+def test_import():
+    """Importing the module worked."""
+    assert vvcli.app
