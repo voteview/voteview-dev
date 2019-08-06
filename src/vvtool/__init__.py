fe9615a8e8af06fc0ec943c9fa9fefc47ea425a2
adamboche <adamboche@gmail.com>
2019-08-06 22:22:14 +0000

    Export objects from app

--- src/vvtool/__init__.py
+++ src/vvtool/__init__.py
@@ -1,1 +1,11 @@
+from .app import Member
+from .app import MemberNokkenPoole
+from .app import MemberNominate
+from .app import Person
+from .app import Rollcall
+from .app import RollcallNominate
+from .app import Vote
+from .app import connect
+
+
 __version__ = "0.1.0"
