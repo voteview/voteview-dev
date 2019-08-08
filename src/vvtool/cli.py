24ccbb85b6ffc33695c0a0346f297a8fcd6d93ff
adamboche <adamboche@gmail.com>
2019-08-08 19:50:11 +0000

    Clean CLI usage

--- src/vvtool/cli.py
+++ src/vvtool/cli.py
@@ -1,75 +1,112 @@
 """Command-line interface for Voteview database."""
 
+import logging
 import pathlib
+import typing as t
 
 import alley
+import attr
 import click
 
 
 PACKAGE_DIR = pathlib.Path(__file__).parent
 
-# pylint: disable=invalid-name
-cli = click.Group(help="Tools for the Voteview database.")
+
+@attr.s(auto_attribs=True, frozen=True, cmp=False)
+class DatabaseInfo:
+    """Database connection data."""
+
+    name: str
+    username: t.Optional[str]
+    password: t.Optional[str]
+    host: t.Optional[str]
+    port: t.Optional[int]
+    auth: t.Optional[str]
 
 
-@cli.group()
-@click.option(
-    "--path",
-    type=click.Path(exists=True, file_okay=False, writable=True, resolve_path=True),
-    default=PACKAGE_DIR,
-)
-@click.option("--database", "-d", required=True, help="The database name")
+@click.group()
+@click.option("--database", "-d", help="The database name")
 @click.option("--username", "-u", help="The database user's username")
 @click.option("--password", "-w", help="The database user's password")
 @click.option("--host", "-h", help="The database host")
 @click.option("--port", "-p", type=int, help="The database port")
 @click.option("--auth", "-a", help="Authentication")
 @click.pass_context
-def migrate(ctx, path, database, username, password, host=None, port=None, auth=None):
+def cli(ctx, database, username, password, host, port, auth):
+    """Tools for the Voteview database."""
+
+    logging.basicConfig(level=logging.DEBUG)
+
+    ctx.obj = {}
+    ctx.obj["db_info"] = DatabaseInfo(
+        name=database,
+        username=username,
+        password=password,
+        host=host,
+        port=port,
+        auth=auth,
+    )
+
+
+@cli.group()
+@click.pass_context
+@click.option(
+    "--path",
+    type=click.Path(exists=True, file_okay=False, writable=True, resolve_path=True),
+    default=PACKAGE_DIR,
+)
+def migrate(ctx, path):
     """Upgrade or downgrade the database with migrations."""
-    ctx.obj = alley.MongoMigrations(
-        path, database, username, password, host=host, port=port, auth=auth
+
+    db = ctx.obj["db_info"]
+
+    if db.name is None:
+        raise ValueError("Database name is required.")
+
+    ctx.obj["migrations_info"] = alley.MongoMigrations(
+        path=path,
+        database=db.name,
+        username=db.username,
+        password=db.password,
+        host=db.host,
+        port=db.port,
+        auth=db.auth,
     )
 
 
 @migrate.command()
-@click.pass_obj
+@click.pass_context
 def status(ctx):
     """Check the current database migration status."""
-    migrations = ctx.migrations
-    migrations.show_status()
+
+    ctx.obj["migrations_info"].migrations.show_status()
 
 
 @migrate.command()
 @click.argument("name")
-@click.pass_obj
+@click.pass_context
 def create(ctx, name):
     """Create a new migration.
 
     Specify a name for the migration.
     """
-    migrations = ctx.migrations
-    migrations.create(name)
+    ctx.obj["migrations_info"].migrations.create(name)
 
 
 @migrate.command()
 @click.argument("migration_id", required=False, type=int)
 @click.option("--dry-run", "fake", is_flag=True, help="Don't actually run it.")
-@click.pass_obj
+@click.pass_context
 def up(ctx, migration_id, fake):
     """Upgrade the database to a specified migration."""
-    migrations = ctx.migrations
-    migrations.up(migration_id, fake)
+
+    ctx.obj["migrations_info"].migrations.up(migration_id, fake)
 
 
 @migrate.command()
 @click.argument("migration_id", type=int)
-@click.pass_obj
+@click.pass_context
 def down(ctx, migration_id):
     """Downgrade the database to a specified migration."""
-    migrations = ctx.migrations
-    migrations.down(migration_id)
 
-
-if __name__ == "__main__":
-    cli()
+    ctx.obj["migrations_info"].migrations.down(migration_id)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -9,9 +9,18 @@ import vvtool
 MIGRATION = importlib.import_module("vvtool.migrations.0001_trump")
 
 
-def run(args, **kwargs):
+def run(db, args, **kwargs):
     """Run vvtool as a subprocess."""
-    return subprocess.check_output(["vvtool"] + args, **kwargs)
+    host, port = db.client.address
+    cli_args = ["vvtool", "-d", db.name, "--host", host, "--port", str(port)]
+    check = kwargs.pop("check", True)
+    return subprocess.run(
+        cli_args + list(args),
+        check=check,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        **kwargs
+    )
 
 
 def test_migrate_cli(db):
@@ -20,8 +29,6 @@ def test_migrate_cli(db):
     Migrating up adds votes to the selected rollcalls.
     Migrating down removes the votes.
     """
-
-    host, port = db.client.address
 
     # Given:
     # The rollcalls on which Trump voted exist.
@@ -33,7 +40,7 @@ def test_migrate_cli(db):
 
     # When:
     # Execute the migration.
-    run(["migrate", "-d", db.name, "--host", host, "--port", str(port), "up", "1"])
+    run(db, ["migrate", "up", "1"])
 
     # Then:
     # Trump's votes appear in the database.
@@ -41,8 +48,45 @@ def test_migrate_cli(db):
 
     # When:
     # Do the down migration.
-    run(["migrate", "-d", db.name, "--host", host, "--port", str(port), "down", "1"])
+    run(db, ["migrate", "down", "1"])
 
     # Then:
     # Trump's votes don't appear in the database.
     assert list(db.voteview_rollcalls.find({"votes.icpsr": 99912})) == []
+
+
+def test_status_empty(db, tmp_path):
+    """The status command shows migrations that haven't been executed."""
+
+    # Given:
+    # An empty migrations/ directory.
+    (tmp_path / "migrations").mkdir()
+
+    # Then:
+    # No migrations are shown.
+    output = run(db, ["migrate", "--path", str(tmp_path), "status"])
+    assert "All migrations registered" in output.stderr.decode()
+
+    # When:
+    # Create a migration.
+    run(db, ["migrate", "--path", str(tmp_path), "create", "first_migration"])
+
+    # Then:
+    # The migration apepars in the status output.
+    output = run(db, ["migrate", "--path", str(tmp_path), "status"]).stderr
+    assert "first_migration" in output.decode()
+
+    # When:
+    # Execute the migration.
+    run(db, ["migrate", "--path", str(tmp_path), "up", "1"])
+
+    # Then:
+    # All migrations have been registered.
+    output = run(db, ["migrate", "--path", str(tmp_path), "status"]).stderr
+    assert "All migrations registered" in output.decode()
+
+
+def test_db_name_required(db):
+    """Database name is required for migrate commands."""
+    proc = subprocess.run(["vvtool", "migrate", "status"], check=False)
+    assert proc.returncode == 1
